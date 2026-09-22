fd9934dbf00971cac1106bdcd90a750da66bba9f
Ganachestheotter <wonggeoff1115@gmail.com>
2026-09-22 21:04:42 +0100

    Update offline cache for GitHub build

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE='history-memory-v0.1.1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./questions.js','./manifest.webmanifest'];
+const CACHE='history-memory-v0.1.1-github';
+const ASSETS=[
+  './','./index.html','./styles.css','./loader.js','./manifest.webmanifest',
+  './payload/app.js.gz.b64',
+  './payload/questions-0.gz.b64',
+  './payload/questions-1.gz.b64'
+];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
